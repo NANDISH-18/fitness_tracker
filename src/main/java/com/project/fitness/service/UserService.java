910af20009868ae2c202d22931575e7524700fc6
NANDISH-18 <nandishmohanty@Nandishs-MacBook-Air.local>
2026-03-13 00:23:35 +0530

    Register Api continue

--- src/main/java/com/project/fitness/service/UserService.java
+++ src/main/java/com/project/fitness/service/UserService.java
@@ -1,4 +1,47 @@
 package com.project.fitness.service;
 
+import com.project.fitness.dto.RegisterRequest;
+import com.project.fitness.dto.UserResponse;
+import com.project.fitness.model.User;
+import com.project.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    public UserResponse register(RegisterRequest registerRequest) {
+        User user = new User(
+                null,
+                registerRequest.getEmail(),
+                registerRequest.getPassword(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+                Instant.parse("2026-12-03T10:15:30.208Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
+                Instant.parse("2026-12-03T10:15:30.208Z").atZone(ZoneOffset.UTC).toLocalDateTime(),
+                List.of(),
+                List.of()
+
+        );
+        User savedUser = userRepository.save(user);
+        return mapToResponse(savedUser);
+    }
+
+    private UserResponse mapToResponse(User savedUser) {
+        UserResponse response = new UserResponse();
+        response.setId(savedUser.getId());
+        response.setEmail(savedUser.getEmail());
+        response.setPassword(savedUser.getPassword());
+        response.setFirstName(savedUser.getFirstName());
+        response.setLastName(savedUser.getLastName());
+        response.setCreatedAt(savedUser.getCreatedAt());
+        response.setUpdatedAt(savedUser.getUpdatedAt());
+        return response;
+    }
 }
